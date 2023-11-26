a2f7f1d15606a2a71c040c1ebc3054c274ad6ba7
Chanuka Samila <chanuka.samila@churchillcap.com>
2023-11-27 00:07:53 +0530

    Added new class for denotes the Error details.

---
+++ CRUD App/Models/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD_App.Models
+{
+    public class ErrorResponse
+    {
+        public string ErrorCode { get; set; } = string.Empty;
+        public string ErrorDescription { get; set; } = string.Empty;
+        public DateTime ErrorGeneratedAt { get; set; }
+    }
+}
