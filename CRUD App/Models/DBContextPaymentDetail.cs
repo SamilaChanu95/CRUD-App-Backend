76350b12d1d535dee8d7511299601721d55b0f9c
Chanuka Samila <chanuka.samila@churchillcap.com>
2023-11-26 21:30:52 +0530

    Added DBContext, Data Model and ConnectionString.

---
+++ CRUD App/Models/DBContextPaymentDetail.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_App.Models
+{
+    public class DBContextPaymentDetail : DbContext
+    {
+        /*private readonly DbContextOptions _options; */
+        public DBContextPaymentDetail(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
