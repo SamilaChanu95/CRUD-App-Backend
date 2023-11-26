b2e32693380188cffee984ebf9499b1f8f5414c8
Chanuka Samila <chanuka.samila@churchillcap.com>
2023-11-27 00:09:36 +0530

    Added a controller in default way of without following any data abstraction.

---
+++ CRUD App/Controllers/PaymentDetailController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CRUD_App.Models;
+
+namespace CRUD_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        private readonly DBContextPaymentDetail _context;
+
+        public PaymentDetailController(DBContextPaymentDetail context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
+        {
+            return await _context.PaymentDetails.ToListAsync();
+        }
+
+        // GET: api/PaymentDetail/5
+        [HttpGet("{id:number}")]
+        public async Task<ActionResult<PaymentDetail>> GetPaymentDetail([FromRoute] int id)
+        {
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+            return paymentDetail;
+        }
+
+        // PUT: api/PaymentDetail/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id:number}")]
+        public async Task<IActionResult> PutPaymentDetail([FromRoute] int id ,[FromQuery] PaymentDetail paymentDetail)
+        {
+            if (id != paymentDetail.PaymentDetailId)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(paymentDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentDetail
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetail>> PostPaymentDetail([FromQuery] PaymentDetail paymentDetail)
+        {
+            _context.PaymentDetails.Add(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetail", new { id = paymentDetail.PaymentDetailId }, paymentDetail);
+        }
+
+        // DELETE: api/PaymentDetail/5
+        [HttpDelete("{id:number}")]
+        public async Task<IActionResult> DeletePaymentDetail([FromRoute] int id)
+        {
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetails.Remove(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentDetailExists(int id)
+        {
+            return _context.PaymentDetails.Any(e => e.PaymentDetailId == id);
+        }
+    }
+}
